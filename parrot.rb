ac7942fa5b163d9c54ca04250e25609a576b801c
Paul Latislaw <paullatislaw@gmail.com>
2020-06-16 04:34:09 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts = "Squawk!"
+def parrot(bird_sound = "Squawk")
+  puts "#{bird_sound}"
+  bird_sounds
 end
 
+parrot()
 
 
   
